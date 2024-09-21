0f1bdbc83d6c36d2ec89a9249a43e478a70b26d5
Oleksii Kyrychenko <a0971720442@gmail.com>
2024-09-21 18:40:16 +0200

    add a division operation

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,25 @@
 #include "calculator.h"
 
-int Calculator::Add (double a, double b)
+double Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
-int Calculator::Sub (double a, double b)
+double Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
-int Calculator::Mul (double a, double b)
+double Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+}
+
+double Calculator::Div (double a, double b)
+{
+    if (b == 0)
+    {
+        throw std::invalid_argument("Division by zero!");
+    }
+    return a / b;
 }
